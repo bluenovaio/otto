6710b45b836a1e89cbda472285bfe3ab7f5b72b7
srosenbauer <seth@bluenova.io>
2021-03-28 18:08:21 -0500

    cleaning up example cli command

--- src/commands/bar.ts
+++
@@ -1,9 +0,0 @@
-export const command = 'bar';
-
-export const describe = 'this is a bar command';
-
-export const builder = {};
-
-export async function handler(argv: any) {
-	console.log('this is something');
-}
--- src/commands/foo.ts
+++
@@ -1,9 +0,0 @@
-export const command = 'foo';
-
-export const describe = 'A description';
-
-export const builder = {};
-
-export async function handler(argv: any) {
-	console.log('foo');
-}
